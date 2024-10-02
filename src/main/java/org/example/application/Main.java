d2f1693ede4895dcbb059149eb7461d91e94501d
Felipe Pedroza <felipelp2910@gmail.com>
2024-10-01 21:08:26 -0300

    Interface comparable

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,31 @@
+package org.example.application;
+
+import org.example.entities.Funcionario;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+        List<Funcionario> list = new ArrayList<>();
+        String path = "src/main/resources/in.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String employeeCsv = br.readLine();
+            while (employeeCsv != null) {
+                String [] fields = employeeCsv.split(",");
+
+                list.add(new Funcionario(fields[0],Double.parseDouble(fields[1])));
+                employeeCsv = br.readLine();
+            }
+            Collections.sort(list);
+            for (Funcionario s : list) {
+                System.out.println(s.getName());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/example/entities/Funcionario.java
@@ -0,0 +1,33 @@
+package org.example.entities;
+
+public class Funcionario  implements  Comparable<Funcionario>{
+
+    private String name;
+    private Double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Funcionario(String name, Double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Funcionario o) {
+        return name.compareTo(o.getName());
+    }
+}
